6e76dfc0911a8097fba5e975a32e9e89628dc9b9
Joe Kariuki <jknguru6@gmail.com>
2023-10-26 09:46:13 +0300

    Add POST method to create a single feedback record

--- src/app/api/feedbacks/route.ts
+++ src/app/api/feedbacks/route.ts
@@ -23,3 +23,38 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.json(json_response);
 }
+
+// POST - add a single feedback record
+export async function POST(request: Request) {
+  try {
+    const json = await request.json();
+
+    const feedback = await prisma.feedback.create({
+      data: json,
+    });
+
+    let json_response = {
+      status: "success",
+      data: {
+        feedback,
+      },
+    };
+
+    return new NextResponse(JSON.stringify(json_response), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    if (error.code === "P2002") {
+      let error_response = {
+        status: "fail",
+        message: "Feedback with the title already exists",
+      };
+
+      return new NextResponse(JSON.stringify(error_response), {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  }
+}

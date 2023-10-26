4aedbffadde04137ebe8c0491870a71a17127d2b
Joe Kariuki <jknguru6@gmail.com>
2023-10-26 13:03:13 +0300

    Add DELETE request for deleting a single feedback record

--- src/app/api/feedbacks/[id]/route.ts
+++ src/app/api/feedbacks/[id]/route.ts
@@ -78,3 +78,39 @@ export async function PATCH(
     });
   }
 }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+    await prisma.feedback.delete({
+      where: { id },
+    });
+
+    return new NextResponse(null, { status: 204 });
+  } catch (error: any) {
+    if (error.code === "P2025") {
+      let error_response = {
+        status: "FAIL",
+        message: "No Feedback with the provided ID found",
+      };
+
+      return new NextResponse(JSON.stringify(error_response), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    let error_response = {
+      status: "ERROR",
+      message: error.message,
+    };
+
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
